bb5b20c4c5c65f678c85e86e44504dda84500f97
Injin Bae <eyegochild@gmail.com>
2020-05-01 16:17:46 +0900

    Add TaskRepository singleton in CompositionRoot

--- app/src/main/java/com/step4me/todo/common/dependencyinjection/CompositionRoot.kt
+++ app/src/main/java/com/step4me/todo/common/dependencyinjection/CompositionRoot.kt
@@ -1,4 +1,39 @@
 package com.step4me.todo.common.dependencyinjection
 
+import android.content.Context
+import androidx.room.Room
+import com.step4me.todo.database.AppDatabase
+import com.step4me.todo.database.TasksRepository
+import com.step4me.todo.database.TasksRepositoryImpl
+
 class CompositionRoot {
+
+    companion object {
+        private const val DB_NAME = "tasks.db"
+    }
+
+    @Volatile
+    private var tasksRepository: TasksRepository? = null
+    @Volatile
+    private var database: AppDatabase? = null
+
+    fun getTasksRepository(context: Context): TasksRepository {
+        val database = database ?: createDatabase(context)
+        return tasksRepository ?: TasksRepositoryImpl(database.tasksDao()).apply {
+            tasksRepository = this
+        }
+    }
+
+    private fun createDatabase(context: Context): AppDatabase {
+        synchronized(this) {
+            val instance = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                DB_NAME
+            ).build()
+            database = instance
+            return instance
+        }
+    }
+
 }
--- app/src/main/java/com/step4me/todo/common/dependencyinjection/ControllerCompositionRoot.kt
+++ app/src/main/java/com/step4me/todo/common/dependencyinjection/ControllerCompositionRoot.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import com.step4me.todo.screens.common.ViewMvcFactory
 import com.step4me.todo.screens.common.screensnavigator.ScreensNavigator
 
-class ControllerCompositionRoot(compositionRoot: CompositionRoot, activity: Activity) {
+class ControllerCompositionRoot(compositionRoot: CompositionRoot,
+                                activity: Activity) {
 
     private val layoutInflater = LayoutInflater.from(activity)
 
     val viewMvcFactory = ViewMvcFactory(layoutInflater)
 
     val screensNavigator = ScreensNavigator(activity)
+
+    val tasksRepository = compositionRoot.getTasksRepository(activity)
+
 }
